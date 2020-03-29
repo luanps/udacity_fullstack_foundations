9516998c9e3888afadabf3af0b5a1b98e3a7202e
luanps <luann.porfirio@gmail.com>
2020-03-28 21:17:44 -0300

    add mocked data and render html templates

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,26 @@
+from flask import render_template
 from app import app
+from datetime import datetime 
 
 @app.route("/ping")
 def ping():
     return "Pong!"
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    user = {'username': 'mocked_user'}
+    posts = [
+        {
+            "author": {"username": "John"},
+            "body": "Beautiful day in Portland!",
+            "time": datetime.now()
+        },
+        {
+            "author": {"username": "Susan"},
+            "body": "The Avengers movie was so cool!" ,
+            "time": datetime.now()
+        }
+    ]
+    return render_template("index.html", title="Home", user=user, posts=posts)
