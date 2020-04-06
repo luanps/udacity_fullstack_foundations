1b492b338080de90930eeeb3c64ef45b29eb4440
Luan Silva <luann.porfirio@gmail.com>
2020-04-05 22:08:44 -0300

    fix redirect page when user is logged

--- app/routes.py
+++ app/routes.py
@@ -47,9 +47,9 @@ def login():
 
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).net.loc != '':
+        if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
-        return redirect(url_for('index'))
+        return redirect(next_page)
 
     return render_template('login.html', title='Sign In', form=form)
 
