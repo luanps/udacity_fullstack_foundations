126ec6b6a67dbf9c22f215b1635d899bc5104c9d
Luan Silva <luann.porfirio@gmail.com>
2020-04-06 22:17:04 -0300

    update last_seen users field with current datetime

--- app/routes.py
+++ app/routes.py
@@ -8,6 +8,13 @@ from app.forms import LoginForm, RegistrationForm
 from app.models import User
 
 
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+
 @app.route("/ping")
 def ping():
     return "Pong!"
