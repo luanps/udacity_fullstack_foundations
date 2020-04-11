4d191fd26cc71fe47a087871b5765223e2ee07ec
Luan Silva <luann.porfirio@gmail.com>
2020-04-10 21:54:40 -0300

    write logs into file

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
+import os
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
 from config import Config
 
 app = Flask(__name__)
@@ -19,6 +20,7 @@ login.login_view = 'login'
 from app import routes, models, errors
 
 if not app.debug:
+    #send logs on email
     if app.config['MAIL_SERVER']:
         auth = None
         if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
@@ -33,3 +35,17 @@ if not app.debug:
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+
+    #write logs into file
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    str_formatter = '''%(asctime)s %(levelname)s: %(message)s
+                [in %(pathname)s: %(lineno)d]'''
+    file_handler.setFormatter(logging.Formatter(str_formatter))
+    file_handler.setLevel(logging.INFO)
+
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
